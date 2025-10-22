fa86235824aff48d1b846323dd32e61a19e03669
Jason Yoder <jason.yoder2@bsu.edu>
2025-10-22 13:29:15 -0400

    Update Picture_test.py

--- tests/Picture_test.py
+++ tests/Picture_test.py
@@ -1,23 +1,9 @@
-from mediaComp.models.Picture import Picture
-from mediaComp.models.PixelColor import Color
+def add_numbers(a, b):
+    return a + b
 
+def test_add_two_numbers():
+    assert add_numbers(2, 3) == 5
 
-
-def test_getExtension():
-    white = Color(255, 255, 255)
-    image = Picture(100, 100, white)
-    assert image.getExtension() == ".jpg"
-    assert image.getExtension() != ".png"
-
-def test_setAllPixelsToAColor():
-    white = Color(255, 255, 255)
-    black = Color(0, 0, 0)
-    image = Picture(100, 100, white)
-    image.setAllPixelsToAColor(black)
-    pixel1 = image.getPixel(0, 0)
-    pixel2 = image.getPixel(50, 50)
-    pixel3 = image.getPixel(99, 99)
-    assert pixel1.getColor().getRGB() ==(0,0,0)
-    assert pixel2.getColor().getRGB() ==(0,0,0)
-    assert pixel3.getColor().getRGB() ==(0,0,0)
+def test_add_negative_numbers():
+    assert add_numbers(-1, -5) == -6
 
